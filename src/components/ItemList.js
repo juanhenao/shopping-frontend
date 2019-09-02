26ce956a99d612985ccb53ada43ffa60c5982e9d
Juan Carlos <jc.hernandez@innovationmecom.de>
2019-09-02 11:53:36 +0200

    Broadcast working!

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -4,12 +4,11 @@ import Pusher from "pusher-js";
 class ItemList extends Component {
   componentDidMount() {
     const pusher = new Pusher("websocketkey", {
-      cluster: "mt1",
       wsHost: "127.0.0.1",
       wsPort: 6001
     });
     const channel = pusher.subscribe("list");
-    channel.bind("itemAdded", data => {
+    channel.bind("item.added", data => {
       console.log(data);
     });
   }
