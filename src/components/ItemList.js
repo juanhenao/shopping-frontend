89458954cde1fb34b3a3adc8abb3735bc811ee93
Juan Carlos <jc.hernandez@innovationmecom.de>
2019-09-02 13:40:55 +0200

    Add Material-UI components

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import Pusher from "pusher-js";
 import axios from "axios";
+import TextField from "@material-ui/core/TextField";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
 class ItemList extends Component {
   state = {
@@ -19,8 +25,8 @@ class ItemList extends Component {
 
   handleList = data => {
     console.log(data);
-    const newList = [...this.state.items, data.item]
-    this.setState({items: newList}, () => console.log(this.state.items));
+    const newList = [...this.state.items, data.item];
+    this.setState({ items: newList }, () => console.log(this.state.items));
   };
 
   handleSubmit = event => {
@@ -29,41 +35,58 @@ class ItemList extends Component {
     const { item } = this.state;
 
     const instance = axios.create({
-      baseURL: 'http://localhost:8000/api/',
+      baseURL: "http://localhost:8000/api/"
     });
 
-    instance.post(`/items`, { item }, {port: 8000}).then(res => {
+    instance.post(`/items`, { item }, { port: 8000 }).then(res => {
       console.log(res);
       console.log(res.data);
     });
+
+    this.setState({item: ''});
   };
 
   handleChange = event => {
     this.setState({ item: event.target.value });
   };
-  
+
   renderList() {
-    const {items} = this.state;
+    const { items } = this.state;
     return (
       <div>
-        <ul>
-          { items.map(item => <li key={item.id}>{item.item}</li>)}
-        </ul>
+        <List component="nav">
+          {items.map(item => {
+            return (
+              <ListItem button>
+                <ListItemText primary={item.item} />
+              </ListItem>
+            );
+          })}
+        </List>
       </div>
     );
   }
 
   render() {
+    const { item } = this.state;
+
     return (
       <div>
         <h1>Shopping List</h1>
         {this.renderList()}
         <form onSubmit={this.handleSubmit}>
           <label>
-            New Item:
-            <input type="text" name="name" onChange={this.handleChange} />
+            <TextField
+              id="standard-name"
+              label="New Item"
+              value={item}
+              onChange={this.handleChange}
+              margin="normal"
+            />
           </label>
-          <button type="submit">Add</button>
+          <Fab color="primary" aria-label="add" type="submit">
+            <AddIcon />
+          </Fab>
         </form>
       </div>
     );
